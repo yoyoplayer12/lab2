ad4bdf43feb9548843ef54bd8e947dd080ef403a
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2023-10-01 18:15:48 +0200

    fix: 'text/css' is not a valid JavaScript MIME type. (deleted css import in main.js)

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 // kickstart your world here
+// import Island from './classes/Island.js';
+// import World from './classes/World.js';
+
+export default class App{
+
+    constructor(){
+        console.log('App constructor');
+        this.hookEvents();
+    }
+
+    hookEvents(){
+       // add event listeners ENTER key on #add-item-text
+         document.querySelector('#btnAddIsland').addEventListener('click', (event) => {
+            console.log('Added Island');
+         });
+
+
+        
+
+
+    }
+
+    //add item to the list
+    
+    // addItem(text){
+    //     console.log(`adding item: ${text}`);
+    //     let todo = new Todo(text);
+    //     todo.render();
+    // }
+
+
+}
